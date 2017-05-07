b9198b0b65f52fe93f3e521d38e81240953dd76e
nuhsat <nuhsatria@gmail.com>
2017-05-07 19:48:07 +0700

    7mei 19:48

--- BackEND/health_history.php
+++ BackEND/health_history.php
@@ -1,19 +1,23 @@
+<!--health history + patient_disease-->
 <?php
     
+
     header('Access-Control-Allow-Origin; *');
     header('Access-Control-Methods; GET, POST, PUT, OPTIONS');
     header('Access-Control-Allow-Header; Content-Type');
 
   include 'db_connect.php';
     $id=$_GET['patient'];
-    $query_user = mysqli_query($connect, "SELECT * FROM health_history JOIN patients WHERE id_pat=id_patient && id_pat='$id'");
+    $query_user = mysqli_query($connect, "SELECT * FROM patients P JOIN health_history H JOIN patients_disease D WHERE id_patient=id_pat AND id_pat='$id' AND H.id_history=D.id_history");
     
+        
+        
     $result_set = array();
     while($result =mysqli_fetch_assoc($query_user)){
         $result_set[]=$result;
     }
 $data =array(
-    'message' => "Get Data User Succses",
+    'message' => "Get Data Health History Succses",
     'data' => $result_set,
     'status' => "200"
 );
