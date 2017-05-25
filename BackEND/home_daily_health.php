8515cd672273a01b47f001d36b9da89c7d2999b6
dwiaditya15 <dwiadityadamayanti10@gmail.com>
2017-05-25 11:11:58 +0700

    25 Mei 2017, update back end

--- BackEND/home_daily_health.php
+++ BackEND/home_daily_health.php
@@ -1,15 +1,17 @@
 <?php
 
+
   include 'db_connect.php';
     $id=$_GET['patient'];
     $query_user = mysqli_query($connect, "SELECT * FROM daily_health_data JOIN patients WHERE id_pat=id_patient && id_pat='$id' ORDER BY  date_daily DESC");
     
     $result_set = array();
     while($result =mysqli_fetch_assoc($query_user)){
+        $i++;
         $result_set[]=$result;
     }
 $data =array(
-    'message' => "Get Data User Succses",
+    'message' => "Get Data Daily Health Succses",
     'data' => $result_set,
     'status' => "200"
 );
