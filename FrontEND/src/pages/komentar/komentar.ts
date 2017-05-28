c3222d2b2d9b96dbb8fcbb39063fe045a081fbc5
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2017-05-29 00:01:27 +0700

    29 Mei 2017, 00:01, komentar DONE

--- FrontEND/src/pages/komentar/komentar.ts
+++ FrontEND/src/pages/komentar/komentar.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { NavController, NavParams,LoadingController } from 'ionic-angular';
 import { Data } from '../../providers/data';
 import { Http } from '@angular/http';
+import { NgForm } from '@angular/forms';
+import { Vibration } from '@ionic-native/vibration';
+import { AlertController } from 'ionic-angular';
+
+var dateNow = new Date();
 
 @Component({
   selector: 'page-komentar',
@@ -12,16 +17,25 @@ export class KomentarPage {
   theme:string;
 
   daily:any;
+  dataKomentar:any;
 
   id_daily_h:number;
   id_patient:number;
+  comment_patient:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public data: Data, public http: Http,public loadCtrl: LoadingController) {
+  name:string;
+
+  submitted = false;
+
+  date:string;
+
+  constructor(public alertCtrl: AlertController ,private vibration: Vibration,public navCtrl: NavController, public navParams: NavParams,public data: Data, public http: Http,public loadCtrl: LoadingController) {
 
     let dataKesehatan = this.navParams.data; //ngambil data yang dikirim
 
     this.id_daily_h = dataKesehatan.id_daily_h;
     this.id_patient = dataKesehatan.id_patient;
+    this.name = dataKesehatan.name_patient;
     
     console.log(dataKesehatan);
   }
@@ -37,10 +51,12 @@ export class KomentarPage {
         content: 'memuat..'
     });
     loading.present();
+    
     this.data.getDataPasien().then((data) => {
       this.theme= data.theme;
 
       this.getDataKesehatan();
+      this.showKomentar();
       
 
     })
@@ -58,6 +74,62 @@ export class KomentarPage {
         
       }
     });
+  }
+
+  showKomentar(){
+    this.http.get(this.data.BASE_URL+"/show_comment.php?id_daily_h="+this.id_daily_h).subscribe(data => {
+      let response = data.json();
+      console.log(response);
+      if(response.status=="200"){
+        this.dataKomentar= response.data;   //ini disimpen ke variabel pasien diatas itu ,, yang udah di delacre
+        
+        
+      }
+    });
+  }
+
+
+  komentar(form: NgForm){
+    
+    let loading = this.loadCtrl.create({
+        content: 'memuat..'
+    });
+    if(form.valid){
+      this.submitted = true;
+      loading.present();
+      let input = JSON.stringify({
+        comment_patient:this.comment_patient,
+        date: dateNow
+      });
+        console.log(input);
+        this.http.post(this.data.BASE_URL+"/comments_patients.php?patient="+this.id_patient+ "&id_daily_h="+this.id_daily_h,input).subscribe(data => {
+        let response = data.json();
+        
+	if(response.status=="200"){
+        console.log(response);    
+        loading.dismiss();
+        this.ionViewWillEnter();
+        form.reset(); //untuk reset input form
+        
+      }
+      
+      else
+           {
+             loading.dismiss();
+             let alert = this.alertCtrl.create({
+                title: 'Proses Gagal',
+                subTitle: 'silahkan coba lagi',      
+                buttons: ['OK']
+              });
+              this.vibration.vibrate(1000);
+              alert.present();
+              
+           }
+
+      });
+    }
+    
+    
   }
 
 
